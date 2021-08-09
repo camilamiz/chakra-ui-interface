f8d179233ffed0a25bc87563e3baa87295ead582
Camila Mizumoto <camila.yuri.m@gmail.com>
2021-08-09 11:25:23 -0300

    style carousel

--- src/components/Carousel.tsx
+++ src/components/Carousel.tsx
@@ -7,7 +7,7 @@ SwiperCore.use([Navigation, Pagination, A11y]);
 
 export function Carousel() {
   return(
-    <Flex w="100%" align="center" justify="center" h="450px" mx="auto" pb="100px">
+    <Flex w="100%" align="center" justify="center" mx="auto" pb="50px">
       <Flex justify="center" w="90%">
         <Swiper
           slidesPerView={1}
@@ -35,10 +35,101 @@ export function Carousel() {
               </Flex>
             </Flex>
           </SwiperSlide>
-          <SwiperSlide>Slide 2</SwiperSlide>
-          <SwiperSlide>Slide 3</SwiperSlide>
-          <SwiperSlide>Slide 4</SwiperSlide>
-          <SwiperSlide>Slide 5</SwiperSlide>
+          <SwiperSlide>
+            <Flex justify="center">
+              <Flex
+                w="100%"
+                h="450px"
+                align="center"
+                justify="center"
+                direction="column"
+                bgImage="url('/africa.jpg')"
+                bgRepeat="no-repeat"
+                bgSize="cover"
+                bgPosition="center"
+                textAlign="center"
+              >
+                <Heading color="gray.100" fontWeight="700" fontSize="48px" pb="16px">Africa</Heading>
+                <Text color="gray.100" fontWeight="700" fontSize="24px">A maior espetáculo de vida selvagem no mundo</Text>
+              </Flex>
+            </Flex>
+          </SwiperSlide>
+          <SwiperSlide>
+            <Flex justify="center">
+              <Flex
+                w="100%"
+                h="450px"
+                align="center"
+                justify="center"
+                direction="column"
+                bgImage="url('/asia.jpg')"
+                bgRepeat="no-repeat"
+                bgSize="cover"
+                bgPosition="center"
+                textAlign="center"
+              >
+                <Heading color="gray.100" fontWeight="700" fontSize="48px" pb="16px">Ásia</Heading>
+                <Text color="gray.100" fontWeight="700" fontSize="24px">Das estepes nômades do Cazaquistão às ruas frenéticas de Hanoi</Text>
+              </Flex>
+            </Flex>
+          </SwiperSlide>
+          <SwiperSlide>
+            <Flex justify="center">
+              <Flex
+                w="100%"
+                h="450px"
+                align="center"
+                justify="center"
+                direction="column"
+                bgImage="url('/northAmerica.jpg')"
+                bgRepeat="no-repeat"
+                bgSize="cover"
+                bgPosition="center"
+                textAlign="center"
+              >
+                <Heading color="gray.100" fontWeight="700" fontSize="48px" pb="16px">América do Norte</Heading>
+                <Text color="gray.100" fontWeight="700" fontSize="24px">Florestas majestosas, desertos e metrópoles pulsantes</Text>
+              </Flex>
+            </Flex>
+          </SwiperSlide>
+          <SwiperSlide>
+            <Flex justify="center">
+              <Flex
+                w="100%"
+                h="450px"
+                align="center"
+                justify="center"
+                direction="column"
+                bgImage="url('/southAmerica.jpg')"
+                bgRepeat="no-repeat"
+                bgSize="cover"
+                bgPosition="center"
+                textAlign="center"
+              >
+                <Heading color="gray.100" fontWeight="700" fontSize="48px" pb="16px">América do Sul</Heading>
+                <Text color="gray.100" fontWeight="700" fontSize="24px">Picos Andinos, floresta amazônica, geleiras da Patagônia, ruínas incas</Text>
+              </Flex>
+            </Flex>
+          </SwiperSlide>
+          <SwiperSlide>
+            <Flex justify="center">
+              <Flex
+                w="100%"
+                h="450px"
+                align="center"
+                justify="center"
+                direction="column"
+                bgImage="url('/oceania.jpg')"
+                bgRepeat="no-repeat"
+                bgSize="cover"
+                bgPosition="center"
+                textAlign="center"
+              >
+                <Heading color="gray.100" fontWeight="700" fontSize="48px" pb="16px">Oceania</Heading>
+                <Text color="gray.100" fontWeight="700" fontSize="24px">Remota, bela e amigável</Text>
+              </Flex>
+            </Flex>
+          </SwiperSlide>
         </Swiper>
       </Flex>
     </Flex>
