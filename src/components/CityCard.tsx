8140edad2580fd645fdf4a67a1474e359d99c500
Camila Mizumoto <camila.yuri.m@gmail.com>
2021-08-09 16:13:09 -0300

    add citycard component

---
+++ src/components/CityCard.tsx
@@ -0,0 +1,39 @@
+import { Box, Img, SimpleGrid, Stack, Text } from "@chakra-ui/react";
+
+type City = {
+  name: string;
+  country: string;
+  image: string;
+  flag: string;
+}
+
+interface CityCardProps {
+  city: City;
+}
+
+export function CityCard({ city }: CityCardProps) {
+  return(
+    <Box w="256px" h="279px" color="yellow.500" border="1px solid" borderRadius="0.2rem" key={city.name}>
+      <Stack>
+        <Img
+          w="256px"
+          h="173px"
+          src={city.image}
+        />
+        <SimpleGrid columns={2} px="25px" pt="1rem" pb="25px">
+          <Box p="">
+            <Text as="p" fontWeight="600" fontSize="20px" fontFamily="Barlow" color="gray.500">{city.name}</Text>
+            <Text as="p" fontWeight="500" fontSize="1rem" fontFamily="Barlow" color="gray.400">{city.country}</Text>
+          </Box>
+          <Box align="right" pt="13px">
+            <Img
+              w="1.875rem"
+              h="1.875rem"
+              src={city.flag}
+            />
+          </Box>
+        </SimpleGrid>
+      </Stack>
+    </Box>
+  );
+}
--- src/pages/continent/[slug].tsx
+++ src/pages/continent/[slug].tsx
@@ -63,28 +63,9 @@ export default function Continent({ continent }: ContinentProps) {
         <Stack>
           <Heading>Cidades +100</Heading>
           <SimpleGrid columns={4} py="2.5rem" spacing="3rem">
-            <Box w="256px" h="279px" color="yellow.500" border="1px solid" borderRadius="0.2rem">
-              <Stack>
-                <Img
-                  w="256px"
-                  h="173px"
-                  src="https://images.unsplash.com/photo-1587726480710-003743795e40?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwxfDB8MXxhbGx8fHx8fHx8fHwxNjIzOTYxNjY3&ixlib=rb-1.2.1&q=80&w=1080&utm_source=unsplash_source&utm_medium=referral&utm_campaign=api-credit"
-                />
-                <SimpleGrid columns={2} px="25px" pt="1rem" pb="25px">
-                  <Box p="">
-                    <Text as="p" fontWeight="600" fontSize="20px" fontFamily="Barlow" color="gray.500">Londres</Text>
-                    <Text as="p" fontWeight="500" fontSize="1rem" fontFamily="Barlow" color="gray.400">Reino Unido</Text>
-                  </Box>
-                  <Box align="right" pt="13px">
-                    <Img
-                      w="1.875rem"
-                      h="1.875rem"
-                      src="https://image.flaticon.com/icons/png/512/197/197374.png"
-                    />
-                  </Box>
-                </SimpleGrid>
-              </Stack>
-            </Box>
+            {continent.cities.map(city => {
+              return(<CityCard city={city} />);
+            })}
           </SimpleGrid>
         </Stack>
       </Stack>
@@ -93,7 +74,6 @@ export default function Continent({ continent }: ContinentProps) {
 }
 
 export const getStaticPaths: GetStaticPaths<{ slug: string }> = async () => {
-
   return {
       paths: [],
       fallback: 'blocking'
